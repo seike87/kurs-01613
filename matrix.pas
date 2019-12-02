bf9085520dc0522e4b207da04f163d0aa27681f9
Stefan <seike@posteo.de>
2019-12-02 11:53:27 +0100

    Beispiel 4.2.1.8

---
+++ matrix.pas
@@ -0,0 +1,58 @@
+program Matrix (input, output);
+{ gibt die Zeilen- und Spaltennummer einer 3x4 Matrix von Integer-Zahlen aus }
+
+    const
+    ZEILENMAX = 3;
+    SPALTENMAX = 4;
+
+    type
+    tZeile = 1..ZEILENMAX;
+    tSpalte = 1..SPALTENMAX;
+    tMatrix = array [tZeile, tSpalte] of integer;
+    tZeilensumme = array [tZeile] of integer;
+    tSpaltensumme = array [tSpalte] of integer;
+
+    var
+    A : tMatrix;
+    B : tZeilensumme;
+    C : tSpaltensumme;
+    i : tZeile;
+    j : tSpalte;
+
+begin
+    { lesen der Matrixwerte in A }
+    for i := 1 to ZEILENMAX do
+        for j := 1 to SPALTENMAX do
+            readln (A[i, j]);
+
+    { Berechnen der Zeilensumme in B }
+    for i := 1 to ZEILENMAX do
+    begin
+        B[i] := 0;
+        for j := 1 to SPALTENMAX do
+            B[i] := B[i] + A[i, j]
+    end;
+
+    { Berechnen der Spaltensumme in C }
+
+    for j := 1 to SPALTENMAX do
+    begin
+        C[j] := 0;
+        for i := 1 to ZEILENMAX do
+            C[j] := C[j] + A[i, j]
+    end;
+
+    { Drucken von A, B und C in geeigneter Form }
+    writeln; { Neue Leerzeile }
+    for i := 1 to ZEILENMAX do
+    begin
+        for j := 1 to SPALTENMAX do
+            write (A[i, j]:5);
+        writeln (B[i]:10)
+    end;
+
+    writeln; { Leerzeile }
+    for j := 1 to SPALTENMAX do
+        write (C[j]:5);
+    writeln
+end; { Matrix }
