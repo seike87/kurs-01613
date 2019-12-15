e99dd66cd0636835fc6eee0095e8e082047345f5
seike <seike@posteo.de>
2019-12-15 15:44:13 +0100

    Klassisches Weihnachtsbaumprogramm

---
+++ weihnachtsbaum.pas
@@ -0,0 +1,34 @@
+program Weihnachtsbaum (input,output);
+{ Stellt nach Eingabe der Höhe und des Zeichens einen Weihnachtsbaum dar }
+
+    const
+    LEER = ' ';
+
+    type
+    tZaehler = 0..maxint;
+
+    var
+    i : tZaehler; { Laufvariable }
+    Hoehe : integer;
+    Zeichen : char;
+
+begin
+
+    write ('Bitte geben Sie die Höhe des Weihnachtsbaums ein: ');
+    readln (Hoehe);
+    write ('Bitte geben Sie das Zeichen ein, aus dem der Weihnachtsbaum erstellt werden soll: ');
+    readln (Zeichen);
+
+    if Hoehe <= 0 then
+        writeln ('Der Weihnachtsbaum muss schon mindestens eine Einheit hoch sein!')
+    else
+    begin
+        writeln(LEER, Zeichen, LEER);
+        for i := 1 to Hoehe do
+            { Zeichen := Zeichen + i; }
+            writeln(LEER, Zeichen, LEER)
+        end
+
+
+
+end. { Weihnachtsbaum }
