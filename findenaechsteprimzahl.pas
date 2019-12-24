84d8dfeeda9d2ec39a520be82f74cce71c7e4b83
Stefan <seike@posteo.de>
2019-12-24 12:06:24 +0100

    Beispiel 4.3.2 - Funktionen

---
+++ findenaechsteprimzahl.pas
@@ -0,0 +1,22 @@
+program FindeNaechstePrimzahl (input, output);
+{ Bestimmt die zur Eingabezahl naechstgelegen(n) Primzahl(en) }
+
+    type
+    tNatZahlPlus = 1..maxint;
+
+    var
+    Einzahl,
+    d : tNatZahlPlus;
+    gefunden : boolean;
+
+
+function istPrimzahl (p : tNatZahlPlus) : boolean;
+
+
+
+end; { istPrimzahl }
+
+begin
+
+
+end. { FindeNaechstePrimzahl }
