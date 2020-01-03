e5297c4386fa3f6050a92e94bf2e85adfec1609d
Stefan <seike@posteo.de>
2020-01-03 21:35:22 +0100

    Beispiel 4.3.2

--- findenaechsteprimzahl.pas
+++ findenaechsteprimzahl.pas
@@ -5,18 +5,61 @@ program FindeNaechstePrimzahl (input, output);
     tNatZahlPlus = 1..maxint;
 
     var
-    Einzahl,
+    EinZahl,
     d : tNatZahlPlus;
     gefunden : boolean;
 
 
 function istPrimzahl (p : tNatZahlPlus) : boolean;
+{ liefert true, falls p Primzahl ist, sonst false }
 
+    var
+    q : tNatZahlPlus;
 
-
+begin
+    if p < 2 then
+        istPrimzahl := false
+    else { p >= 2 }
+    begin
+        istPrimzahl := true;
+        for q := 2 to p - 1 do
+            if p mod q = 0 then
+                istPrimzahl := false
+    end;
 end; { istPrimzahl }
 
 begin
+    writeln ('Zahl eingeben: ');
+    readln(EinZahl);
 
-
+    write ('Naechste Primzahl zu ', EinZahl, ' ist ');
+    if istPrimzahl (EinZahl) then
+        writeln (EinZahl)
+    else
+        if EinZahl = 1 then
+            writeln ('2':5)
+        else { EinZahl <> 1 }
+        begin
+            gefunden := false;
+            if odd (EinZahl) then
+                d := 2
+            else
+                d := 1;
+            repeat
+                if istPrimzahl (EinZahl + d) then
+                begin
+                    { Primzahl oberhalb von EinZahl gefunden }
+                    gefunden := true;
+                    write (EinZahl + d : 5)
+                end;
+                if istPrimzahl (EinZahl - d) then
+                begin
+                    { Primzahl unterhalb von EinZahl gefunden }
+                    gefunden := true;
+                    write (EinZahl - d : 5)
+                end;
+                d := d + 2
+            until gefunden;
+            writeln;
+        end { EinZahl <> 1 }
 end. { FindeNaechstePrimzahl }
