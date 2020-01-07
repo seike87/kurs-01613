1e01c1ccd68b6463221f0139a55b73b90048e4ee
Stefan <seike@posteo.de>
2020-01-07 09:50:22 +0100

    Created tictactoe.pas

---
+++ tictactoe.pas
@@ -0,0 +1,19 @@
+program tictactoe (input, output);
+
+    const
+    ZEILENMAX = 3;
+    SPALTENMAX = 3;
+    LWAAGERECHT = '-----------';
+    LSENKRECHT = '|';
+
+    type
+    tZeile = 1..ZEILENMAX;
+    tSpalte = 1..SPALTENMAX;
+    tMatrix = array [tZeile, tSpalte] of char; { Spielfeld }
+
+
+
+
+
+
+end. { tictactoe }
